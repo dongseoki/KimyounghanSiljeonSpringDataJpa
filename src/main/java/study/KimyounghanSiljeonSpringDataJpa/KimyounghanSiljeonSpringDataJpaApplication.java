9fba0f05490f34e0aa501010f48a6e34707d007e
Dong Seok Lee <dongseok.lee.log@gmail.com>
2023-09-23 13:31:03 +0900

    first commit

---
+++ src/main/java/study/KimyounghanSiljeonSpringDataJpa/KimyounghanSiljeonSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package study.KimyounghanSiljeonSpringDataJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimyounghanSiljeonSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimyounghanSiljeonSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/KimyounghanSiljeonSpringDataJpa/KimyounghanSiljeonSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.KimyounghanSiljeonSpringDataJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimyounghanSiljeonSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
