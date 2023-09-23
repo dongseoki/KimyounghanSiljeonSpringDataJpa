ee4bbb4658410e6ffbb0bde0db6b5d37564216a1
Dong Seok Lee <dongseok.lee.log@gmail.com>
2023-09-23 20:16:54 +0900

    섹션 2. 예제 도메인 모델: 예제 도메인 모델과 동작확인

---
+++ src/main/java/study/KimyounghanSiljeonSpringDataJpa/entity/Team.java
@@ -0,0 +1,34 @@
+package study.KimyounghanSiljeonSpringDataJpa.entity;
+
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "team_id")
+  private Long id;
+  private String name;
+
+  @OneToMany(mappedBy = "team")
+  List<Member> members = new ArrayList<>();
+
+
+  public Team(String name) {
+    this.name = name;
+  }
+
+}
