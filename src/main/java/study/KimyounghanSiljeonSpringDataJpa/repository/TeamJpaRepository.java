9358d075463e4455b056ce92fed619c012a6576c
dongseoki <43495319+dongseoki@users.noreply.github.com>
2023-09-24 20:32:57 +0900

    섹션 3. 공통 인터페이스 기능: 순수 JPA 기반 리포지토리 만들기

---
+++ src/main/java/study/KimyounghanSiljeonSpringDataJpa/repository/TeamJpaRepository.java
@@ -0,0 +1,45 @@
+package study.KimyounghanSiljeonSpringDataJpa.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import study.KimyounghanSiljeonSpringDataJpa.entity.Team;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamJpaRepository {
+
+  @PersistenceContext
+  private EntityManager em;
+
+
+  public Team save(Team team) {
+    em.persist(team);
+    return team;
+  }
+
+
+  public void delete(Team team) {
+    em.remove(team);
+  }
+
+
+  public List<Team> findAll() {
+    return em.createQuery("select t from Team t", Team.class)
+             .getResultList();
+  }
+
+
+  public long count() {
+    return em.createQuery("select count(t) from Team t", Long.class)
+             .getSingleResult();
+  }
+
+
+  public Optional<Team> findById(Long id) {
+    return Optional.ofNullable(em.find(Team.class, id));
+  }
+
+}
--- src/test/java/study/KimyounghanSiljeonSpringDataJpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/KimyounghanSiljeonSpringDataJpa/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.KimyounghanSiljeonSpringDataJpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 //import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,11 +27,35 @@ class MemberJpaRepositoryTest {
     Member member = new Member("memberA");
     Member saveMember = memberJpaRepository.save(member);
 
-    Member findMember = memberJpaRepository.findById(saveMember.getId());
+    Member findMember = memberJpaRepository.findById(saveMember.getId()).get();
 
     assertThat(findMember.getId()).isEqualTo(member.getId());
     assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
     assertThat(findMember).isEqualTo(member);
+  }
+
+  @Test
+  void testCRUD() {
+    Member member1 = new Member("member1");
+    Member member2 = new Member("member2");
+    memberJpaRepository.save(member1);
+    memberJpaRepository.save(member2);
+
+    Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+    Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+    assertThat(findMember1.getId()).isEqualTo(member1.getId());
+    assertThat(findMember2.getId()).isEqualTo(member2.getId());
+
+    List<Member> members = memberJpaRepository.findAll();
+    assertThat(members.size()).isEqualTo(2);
+
+    long count = memberJpaRepository.count();
+    assertThat(count).isEqualTo(2);
+
+    memberJpaRepository.delete(member1);
+    memberJpaRepository.delete(member2);
+    long deletedCount = memberJpaRepository.count();
+    assertThat(deletedCount).isEqualTo(0);
   }
 
 }
